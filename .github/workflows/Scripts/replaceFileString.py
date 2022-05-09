6541b9be21cf2fb5febb7f87dd30891b23e0272d
MioxZ <45838879+MioxZ@users.noreply.github.com>
2022-05-09 17:53:21 +0800

    Create replaceFileString.py

---
+++ .github/workflows/Scripts/replaceFileString.py
@@ -0,0 +1,26 @@
+# libraries
+import sys
+
+# Variables Initialization 
+fileName = sys.argv[1];
+existingString = sys.argv[2];
+newString = sys.argv[3];
+data = None;
+
+# Main
+print("Running script - {0}...".format(sys.argv[0]));
+print("To read from - {0}".format(sys.argv[1]));
+print("To replace - {0}".format(sys.argv[2]));
+print("To replace with - {0}".format(sys.argv[3]));
+
+with open(fileName, 'rt') as readFile:
+    if (existingString in readFile.read()):
+        data = readFile.read();
+        data = data.replace(existingString, newString);
+    else:
+        print("String is not found.");
+
+if (len(data)):
+    with open(fileName, 'wt') as writeFile:
+        writeFile.write(data);
+        print("Replaced successfully.");
